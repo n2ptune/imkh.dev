036d9e986e038089ef02f6a8cc23d8ccaf6b583a
n2ptune <n2ptune@outlook.com>
2020-02-09 14:57:55 +0900

    defaults page query

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -10,6 +10,27 @@ import Layout from '@/layouts/Default.vue'
 export default {
   components: {
     Layout
+  },
+  mounted() {
+    console.log(this.$page.post)
   }
 }
 </script>
+
+<page-query>
+query Post ($id: ID!) {
+  post: post (id: $id) {
+    title
+    path
+    date (format: "D. MMMM. YYYY")
+    timeToRead
+    tags {
+      id
+      title
+      path
+    }
+    description
+    content
+  }
+}
+</page-query>
