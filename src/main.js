5a2f56a103e40baf5a7516f376a1d78afc835f28
n2ptune <n2ptune@outlook.com>
2020-02-09 16:16:06 +0900

    changed: default html attributes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import '@/styles/global.css'
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function(Vue, { router, head, isClient }) {
+  head.htmlAttrs = { lang: 'ko' }
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
