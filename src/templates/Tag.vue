fcc56503323cac91e511005339b3a4ba5354133a
n2ptune <n2ptune@outlook.com>
2020-02-09 15:16:47 +0900

    added: Tag Components

---
+++ src/templates/Tag.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+    asdff
+  </Layout>
+</template>
+
+<script>
+import Layout from '@/layouts/Default.vue'
+
+export default {
+  components: {
+    Layout
+  },
+  mounted() {
+    console.log(this.$page.tag)
+  }
+}
+</script>
+
+<page-query>
+query Tag ($id: ID!) {
+  tag (id: $id) {
+    title
+    belongsTo {
+      edges {
+        node {
+          ...on Post {
+            title
+            path
+            date (format: "D. MMMM YYYY")
+            timeToRead
+            description
+            content
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
