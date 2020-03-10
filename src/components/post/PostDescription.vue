cbe4ce1d705a703687d117048c01e6eb284b5254
n2ptune <n2ptune@outlook.com>
2020-03-10 12:33:04 +0900

    fix: specific icon size

--- src/components/post/PostDescription.vue
+++ src/components/post/PostDescription.vue
@@ -1,7 +1,10 @@
 <template>
   <article class="my-4 bg-red-200 full-width">
     <header class="py-2 px-4 bg-red-300">
-      <font-awesome :icon="['fas', 'highlighter']" class="text-gray-800" />
+      <font-awesome
+        :icon="['fas', 'highlighter']"
+        class="text-gray-800 text-base"
+      />
       <span class="text-sm font-semibold ml-2 text-gray-800">Description</span>
     </header>
     <article class="text-base px-4 py-4 md:px-8">
