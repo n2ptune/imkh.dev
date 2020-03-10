4e31093db03bdb50581afcc35ba9c5cddac8d4a2
n2ptune <n2ptune@outlook.com>
2020-03-10 18:21:43 +0900

    add: gridsome svg icon in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,18 @@
   <footer
     class="flex flex-col justify-center items-center relative bottom-0 w-full bg-gray-800 px-2 py-4 sm:px-6"
   >
-    <div class="text-white-f text-base my-2">
+    <div class="text-white-f text-lg">
       (C) 2020. n2ptune
+    </div>
+    <div class="text-white-500 mb-2">
+      <div class="inline-block align-middle">
+        <span>Made with </span>
+        <g-image
+          src="@/assets/gridsome_icon.svg"
+          class="inline-block align-top opacity-75"
+          width="24"
+        />
+      </div>
     </div>
     <div class="my-2">
       <a href="https://github.com/n2ptune" target="_blank">
