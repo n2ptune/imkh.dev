1bd7f2ca686d2b44f7a6114df8268cc6cc321c2b
n2ptune <n2ptune@outlook.com>
2020-11-27 11:51:41 +0900

    fix: separate component that tag list

--- src/components/layouts/main/PostCard.vue
+++ src/components/layouts/main/PostCard.vue
@@ -20,19 +20,7 @@
           <div class="text-base text-white-400 mt-5 desc">
             {{ post.cover_image ? post.description : post.excerpt + '...' }}
           </div>
-          <div class="mt-3">
-            <ul class="space-x-2 space-y-1">
-              <li
-                v-for="tag in sortedTags"
-                :key="tag.id"
-                class="inline-block rounded py-1 px-3 text-sm transition-colors duration-300 text-white-500 bg-elevation-500 hover:bg-elevation-800 hover:text-white-800"
-              >
-                <g-link :to="tag.path">
-                  {{ tag.id }}
-                </g-link>
-              </li>
-            </ul>
-          </div>
+          <tag-list :tag="post.tags" />
         </div>
       </div>
     </g-link>
@@ -53,21 +41,17 @@
 
 <script>
 import TimeReadMixins from '@/components/mixins/TimeReadMixins'
+import TagList from '@/components/tag/List.vue'
 
 export default {
+  components: {
+    TagList
+  },
+
   props: {
     post: {
       type: Object,
       required: true
-    }
-  },
-
-  computed: {
-    sortedTags() {
-      const tags = this.post.tags.slice()
-      tags.sort((a, b) => (a.id > b.id ? 1 : -1))
-
-      return tags
     }
   },
 
