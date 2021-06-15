5c693924268814acf2ca20dacb1bd9cf0c7ce826
n2ptune <n2ptune@outlook.com>
2021-06-16 00:04:19 +0900

    fix: fetch local markdown data on page component

--- src/pages/{MarkdownRemark.fields__slug}.tsx
+++ src/pages/{MarkdownRemark.fields__slug}.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import PostLayout from '@/components/layouts/PostLayout'
-import { PageProps } from 'gatsby'
-import { usePostWithSlug } from '@/hooks/posts'
+import { graphql, PageProps } from 'gatsby'
 
-export default function Post({ pageContext }: PageProps) {
-  const data = usePostWithSlug((pageContext as any).fields__slug)
-  console.log(data)
-  return <PostLayout>{'Hello World'.repeat(20)}</PostLayout>
+export default function Post(props: PageProps) {
+  console.log(props)
+  const { markdownRemark: md } = props.data as any
+  return (
+    <PostLayout>
+      <div className="text-3xl font-bold">{md.frontmatter.title}</div>
+      <div dangerouslySetInnerHTML={{ __html: md.html }}></div>
+    </PostLayout>
+  )
 }
+
+export const query = graphql`
+  query ($fields__slug: String) {
+    markdownRemark(fields: { slug: { eq: $fields__slug } }) {
+      id
+      tableOfContents(absolute: false, heading: "", maxDepth: 10)
+      timeToRead
+      html
+      frontmatter {
+        date
+        description
+        published
+        tags
+        title
+      }
+      fields {
+        slug
+      }
+    }
+  }
+`
