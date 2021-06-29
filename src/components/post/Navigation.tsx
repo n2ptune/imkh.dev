1d40855d54ddc9d82bca1b960d0e1676ce9e66d3
n2ptune <n2ptune@outlook.com>
2021-06-29 21:30:20 +0900

    fix(style): working on navigation styles

--- src/components/post/Navigation.tsx
+++ src/components/post/Navigation.tsx
@@ -2,6 +2,7 @@ import { SinglePostContext } from '@/context/post'
 import React, { useContext } from 'react'
 import * as styleModule from '@/styles/post-navi.module.css'
 import classNames from 'classnames'
+import { BsCardList } from 'react-icons/bs'
 
 export type TableOfContents = {
   depth: number
@@ -14,20 +15,21 @@ const Navigation: React.FC = () => {
   const depthClass = {
     '3': 'pl-3'
   }
+  const itemClass = classNames('transition-all duration-200')
 
   return (
     <aside className="hidden lg:block lg:col-span-3 lg:pl-6 xl:pl-8">
       <div className="sticky top-32 text-sm">
-        <span>목차</span>
+        <span className="space-x-2">
+          <BsCardList className="inline-block text-base" />
+          <span className="font-bold text-sm">목차</span>
+        </span>
         {post && (
           <ul className={styleModule.navi}>
             {(post.headings as TableOfContents[]).map(heading => (
               <li
                 key={heading.id}
-                className={classNames(
-                  styleModule.item,
-                  depthClass[heading.depth]
-                )}
+                className={classNames(depthClass[heading.depth], itemClass)}
               >
                 <a href={`#${heading.id}`}>{heading.value}</a>
               </li>
