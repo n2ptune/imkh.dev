5966117cafe1275d6871c2cf828c52df60ddf3da
n2ptune <n2ptune@outlook.com>
2021-09-19 16:00:38 +0900

    fix(memo): base layout memo template

--- src/templates/Memo.vue
+++ src/templates/Memo.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Memo</div>
+  <DefaultLayout>
+    <section class="wrapper">
+      asdasf
+    </section>
+  </DefaultLayout>
 </template>
 
 <script>
@@ -9,3 +13,39 @@ export default {
   components: { DefaultLayout }
 }
 </script>
+
+<page-query>
+query Memo ($id: ID!) {
+  memo: memo (id: $id) {
+    content
+  }
+}
+</page-query>
+
+<style lang="postcss" scoped>
+.wrapper {
+  @apply relative mx-auto mb-32 py-10 rounded-none text-white-800 px-4
+  overflow-x-hidden;
+
+  max-width: 100%;
+  top: 7rem;
+
+  & .cover-image {
+    > img {
+      @apply mx-auto max-w-full overflow-hidden;
+    }
+  }
+}
+
+@screen md {
+  .wrapper {
+    @apply px-0;
+  }
+}
+
+@screen lg {
+  .wrapper {
+    max-width: 1100px;
+  }
+}
+</style>
