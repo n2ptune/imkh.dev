5a9f04ca7c9e00c632ca6396bdd9f2c381cb3322
n2ptune <n2ptune@outlook.com>
2021-10-29 20:00:51 +0900

    fix: memo list layout;

--- src/components/note/ListItem.vue
+++ src/components/note/ListItem.vue
@@ -1,24 +1,22 @@
 <template>
-  <g-link :to="noteItem.path" class="w-full group">
-    <div class="space-y-1">
-      <div class="flex justify-between">
-        <div>
-          <span class="mr-2">
-            <font-awesome
-              class="text-white-500 group-hover:text-white-800 transition-colors duration-300"
-              :icon="['far', 'sticky-note']"
-            />
-          </span>
-        </div>
+  <g-link :to="noteItem.path" class="wrapper">
+    <div class="space-y-1 relative px-6 pt-4 pb-12">
+      <div class="icon-wrap">
+        <font-awesome
+          class="text-white-500 group-hover:text-white-800 transition-colors duration-300"
+          :icon="['far', 'sticky-note']"
+        />
+      </div>
+      <div class="flex">
         <div
-          class="text-sm text-white-500 group-hover:text-white-800 transition-colors duration-300"
+          class="ml-auto text-sm text-white-500 group-hover:text-white-800 transition-colors duration-300"
         >
           {{ noteItem.date }}
         </div>
       </div>
       <div class="text-xl font-bold">{{ noteItem.title }}</div>
       <div
-        class="text-white-500 group-hover:text-white-800 transition-colors duration-300"
+        class="text-white-500 group-hover:text-white-800 transition-colors duration-300 font-thin"
       >
         {{ noteItem.excerpt }}
       </div>
@@ -36,3 +34,17 @@ export default {
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+.wrapper {
+  @apply w-full group rounded-lg border-elevation-200 border
+  hover:border-elevation-900 transition-colors duration-300;
+
+  .icon-wrap {
+    top: -16px;
+    left: 15px;
+
+    @apply absolute bg-dark-surface px-2;
+  }
+}
+</style>
