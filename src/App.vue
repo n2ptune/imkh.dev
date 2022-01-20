b33d55e8005ba6392dfc7d8a275fc0d4993b4bef
n2ptune <n2ptune@outlook.com>
2022-01-20 20:11:57 +0900

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="layout">
-    <router-view />
-    <SearchDialog v-if="isSearchDialog" />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<script>
-import { EventBus } from '@/components/utils/EventBus'
-import SearchDialog from '@/components/search/SearchDialog.vue'
-
-export default {
-  data: () => ({
-    isSearchDialog: false
-  }),
-
-  components: {
-    SearchDialog
-  },
-
-  created() {
-    EventBus.$on('search', e => (this.isSearchDialog = e))
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-</script>
+</style>
