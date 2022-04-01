510736836981b6dc981d2be148488528c0f9beb7
n2ptune <n2ptune@outlook.com>
2022-04-01 21:27:57 +0900

    routes test

---
+++ src/routes/posts/a.md
@@ -0,0 +1,5 @@
+---
+title: 1234
+---
+
+asfasfqwfqwf
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
 
@@ -16,8 +16,18 @@ const config = {
 		})
 	],
 
+	extensions: ['.md', '.svelte'],
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		prerender: {
+			default: true
+		}
 	}
 }
 
