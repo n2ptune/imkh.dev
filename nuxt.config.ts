2201ee803633fe3b5e07b3c2d551aa5900c214c6
n2ptune <n2ptune@outlook.com>
2023-02-04 17:18:45 +0900

    정리

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,5 @@ import path from 'node:path'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxt/image-edge'],
-  content: {
-    sources: {
-      post: {
-        driver: 'fs',
-        prefix: '/',
-        base: path.resolve(__dirname, 'content/posts')
-      }
-    }
-  },
-  generate: {},
-  srcDir: 'src/'
+  modules: ['@nuxt/content']
 })
---
+++ pages/[post].vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const route = useRoute()
+const query = queryContent('posts')
+const content = ref<any>(null)
+
+query
+  .where({ _path: ('/posts/' + route.params.post) as string })
+  .findOne()
+  .then(result => (content.value = result))
+</script>
+
+<template>
+  <main>
+    <ContentRenderer v-if="content" :value="content" />
+    <div v-else>
+      Not Found
+    </div>
+  </main>
+</template>
