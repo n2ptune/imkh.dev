f1441b0668cc2c8308d4ae7c92292a95ae48949e
n2ptune <n2ptune@outlook.com>
2023-02-24 00:30:14 +0900

    feat: pagination (query hack)

--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,36 @@ definePageMeta({
   layout: 'list-layout'
 })
 
-const postSkip = ref(0)
-const query = queryContent('posts')
-const posts = ref<Pick<ParsedContent, string>[]>([])
+const __PAGE_SIZE__ = 10
 
-async function getPosts() {
-  const queryPosts = await query
-    .limit(10)
-    .skip(postSkip.value)
-    .only(['title', 'id', 'excerpt'])
-    .sort({ created_at: -1 })
+const { data } = await useAsyncData('getPosts', () =>
+  queryContent('posts')
+    .only(['title', 'id', 'excerpt', 'date'])
+    .sort({ date: -1 })
     .find()
-  posts.value = queryPosts
-}
+)
 
-getPosts()
+const currentPage = ref(0)
+const posts = computed(() => {
+  const start = 0
+  const end = currentPage.value * __PAGE_SIZE__ + __PAGE_SIZE__
+  return data.value?.slice(start, end)
+})
+const allLoaded = computed(() => {
+  if (!data.value) return false
+
+  return data.value.length / __PAGE_SIZE__ - 1 <= currentPage.value
+})
+
+function getPosts() {
+  if (allLoaded.value) return
+  currentPage.value++
+}
 </script>
 
 <template>
   <section class="w-full">
-    <PostList :posts="posts" />
+    <PostList :posts="(posts as any[])" />
+    <button v-if="!allLoaded" @click="getPosts">load more</button>
   </section>
 </template>
