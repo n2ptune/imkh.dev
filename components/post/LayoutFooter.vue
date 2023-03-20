84e00d4b19beb3cf4a495d780a4880626f738014
n2ptune <n2ptune@outlook.com>
2023-03-21 01:06:52 +0900

    chore: version up for nuxt dependencies, style for footer

--- components/post/LayoutFooter.vue
+++ components/post/LayoutFooter.vue
@@ -1,5 +1,41 @@
 <template>
-  <footer class="w-full mt-12 py-24 bg-dark">
-    <section class="container mx-auto">asdf</section>
+  <footer
+    class="w-full mt-12 pt-12 pb-24 lg:pt-16 lg:pb-36 bg-gray-50 border-t border-gray-200 dark:bg-[#0f0f0f] dark:border-none"
+  >
+    <section class="container mx-auto">
+      <section class="grid-wrap">
+        <section class="col-span-4 text-xl">DEVLOG 개발 블로그</section>
+        <section class="col-span-8 grid-wrap">
+          <section class="col-span-4 mx-auto">
+            <p class="head">heading</p>
+            <ul>
+              <li>a-1</li>
+              <li>a-2</li>
+            </ul>
+          </section>
+          <section class="col-span-4 mx-auto">
+            <p class="head">heading</p>
+          </section>
+          <section class="col-span-4 mx-auto">
+            <p class="head">heading</p>
+          </section>
+        </section>
+      </section>
+      <section class="mt-24 text-center text-2xl">Nuxt</section>
+    </section>
   </footer>
 </template>
+
+<style lang="postcss" scoped>
+.grid-wrap {
+  @apply grid grid-cols-12;
+
+  p.head {
+    @apply mb-4;
+  }
+
+  ul {
+    @apply leading-relaxed;
+  }
+}
+</style>
--- pages/[post].vue
+++ pages/[post].vue
@@ -3,6 +3,16 @@ definePageMeta({
   layout: 'post-layout'
 })
 
+useHead({
+  script: [
+    {
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3441377677018772',
+      async: true,
+      crossorigin: 'anonymous'
+    }
+  ]
+})
+
 const route = useRoute()
 const query = queryContent('posts')
 const data = await query
