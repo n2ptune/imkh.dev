b13583b01f7e6daf06d9476ed241ca29bf593021
n2ptune <n2ptune@outlook.com>
2023-05-16 23:52:56 +0900

    gtm

--- src/layouts/SEOWrapper.vue
+++ src/layouts/SEOWrapper.vue
@@ -14,6 +14,14 @@ useHead({
       src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3441377677018772',
       async: true,
       crossorigin: 'anonymous'
+    },
+    {
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W4HRXVM');`,
+      tagPosition: 'head'
     }
   ]
 })
