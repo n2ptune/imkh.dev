b818cf7bceaa74cfde7b76d0567bf6fd8b61c418
n2ptune <n2ptune@outlook.com>
2023-06-23 15:29:15 +0900

    fix: remove style by adsbygoogle dataset

--- src/components/ads/ArticleAds.vue
+++ src/components/ads/ArticleAds.vue
@@ -6,7 +6,7 @@ onMounted(() => {
 
 <template>
   <ins
-    class="adsbygoogle animate-pulse my-12 sk prose lg:prose-lg mx-auto"
+    class="adsbygoogle"
     style="display: block; text-align: center"
     data-ad-layout="in-article"
     data-ad-format="fluid"
@@ -14,3 +14,13 @@ onMounted(() => {
     data-ad-slot="6068611830"
   ></ins>
 </template>
+
+<style lang="postcss" scoped>
+ins.adsbygoogle {
+  @apply rounded-lg my-12 prose lg:prose-lg mx-auto;
+
+  &:not([data-ad-status='filled']) {
+    @apply animate-pulse bg-slate-100 dark:bg-neutral-900;
+  }
+}
+</style>
