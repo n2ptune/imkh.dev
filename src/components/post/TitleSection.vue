9ca8e5ad663c974b63946979c03c315ba45f9408
n2ptune <n2ptune@outlook.com>
2024-01-12 01:45:52 +0900

    fix: truncate title section in post layout

--- src/components/post/TitleSection.vue
+++ src/components/post/TitleSection.vue
@@ -14,14 +14,14 @@ const dateFormat = computed(() =>
 </script>
 
 <template>
-  <section class="text-center mb-12 lg:mb-24">
+  <section class="container mx-auto max-w-4xl text-center mb-12 lg:mb-24">
     <p class="text-4xl font-black">{{ props.title }}</p>
     <p
       class="italic mt-2 space-x-2 text-gray-400 dark:text-gray-500 font-light"
     >
       <span>{{ dateFormat }}</span>
       <span>:</span>
-      <span v-if="tags && tags.length" class="hidden lg:inline-block">
+      <span v-if="tags && tags.length" class="inline-block">
         {{ tags.join(', ') }}
       </span>
     </p>
