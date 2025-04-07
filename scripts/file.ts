18954c45c7f8cc40ead5b009eaca8e38db8c860c
n2ptune <n2ptune@outlook.com>
2025-04-07 17:57:06 +0900

    feat: add script for create post template file

--- scripts/file.ts
+++ scripts/file.ts
@@ -1,5 +1,6 @@
-import { readdirSync, readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
+import { readdirSync, readFileSync, writeFileSync } from 'node:fs'
+import { resolve, join } from 'node:path'
+import inq from 'inquirer'
 
 const postPath = resolve(__dirname, '../src/content/posts')
 const postDir = readdirSync(postPath)
@@ -41,3 +42,46 @@ export function getRoutesByTags() {
 
   return tagResult.map(tag => `/tag/${tag}`)
 }
+
+inq
+  .prompt([
+    {
+      message: 'File name',
+      name: 'filename',
+      type: 'input',
+      default: 'default'
+    },
+    { message: 'Title', name: 'title', type: 'input', default: 'Test Title' },
+    { message: 'Published', name: 'published', type: 'confirm', default: true },
+    {
+      message: 'Tags',
+      name: 'tags',
+      type: 'input',
+      default: ''
+    },
+    {
+      message: 'Description',
+      name: 'description',
+      type: 'input',
+      default: ''
+    }
+  ])
+  .then(answers => {
+    const { published, tags, title, description, filename } = answers
+    const cover_image = ''
+    const date = new Date().toISOString()
+
+    writeFileSync(
+      join(postPath, filename + '.md'),
+      `---
+title: ${title}
+published: ${published}
+date: ${date}
+cover_image: ${cover_image}
+description: ${description}
+tags: ${tags}
+---
+
+## ${title}`
+    )
+  })
