ec4206134e03c790e45b6ea1a78c4431e1892f69
n2ptune <n2ptune@outlook.com>
2026-01-14 13:05:34 +0900

    remove border under lg breakpoints

--- src/widgets/layouts/short.vue
+++ src/widgets/layouts/short.vue
@@ -10,7 +10,7 @@ import { SEOWrapper } from '../seo'
     <Header />
     <LayoutMain class="!mt-8 !pb-0">
       <section
-        class="lg:max-w-3xl mx-auto border-x border-neutral-200 dark:border-neutral-800 py-16 px-8"
+        class="lg:max-w-3xl mx-auto border-0 lg:border-x border-neutral-200 dark:border-neutral-800 py-16 px-8"
       >
         <slot />
       </section>
